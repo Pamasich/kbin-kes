a29085d7f8e76d760822d79845e4f79f5f4487e8
Pamasich <pamasich@gmail.com>
2024-01-25 14:42:39 +0100

    created remaining tests for alt-all-content-access

--- test/alt-all-content-access.mjs
+++ test/alt-all-content-access.mjs
@@ -6,34 +6,150 @@ import { setup } from "./api.mjs";
 /** @type {Document} */
 let document = undefined;
 
+const modId = "alt-all-content-access";
+const modClass = "AlternativeAllContentAccessMod";
+
 describe ("alt-all-content-access", function () {
     beforeEach (async function () {
-        document = await setup("alt-all-content-access", "AlternativeAllContentAccessMod");
+        document = await setup(modId, modClass);
     })
 
     describe ("setButtonVisibility", function () {
-        it ("should abort if the setting is turned off", function () {
-            document.mod.getHideButtonSetting = () => false;
-            const dummyButton = document.createElement("div");
-            document.mod.getAllContentButton = () => dummyButton;
-            document.mod.setButtonVisibility (true);
-            assert.notEqual(dummyButton.style.display, "none");
-        })
-
-        it ("should hide the button if the mod is active", function () {
+        it ("should hide the button while the setting is turned on", function () {
             document.mod.getHideButtonSetting = () => true;
             const dummyButton = document.createElement("div");
             document.mod.getAllContentButton = () => dummyButton;
-            document.mod.setButtonVisibility (true);
+            document.mod.setButtonVisibility(true);
             assert.equal(dummyButton.style.display, "none");
         })
-
-        it ("should reset the button if the mod is turned off", function () {
+        it ("should show the button again after the setting has been turned off", function () {
             document.mod.getHideButtonSetting = () => true;
             const dummyButton = document.createElement("div");
             document.mod.getAllContentButton = () => dummyButton;
-            document.mod.setButtonVisibility (false);
+            document.mod.setButtonVisibility(true);
+            document.mod.getHideButtonSetting = () => false;
+            document.mod.setButtonVisibility(true);
+            assert.notEqual(dummyButton.style.display, "none");
+        })
+        it ("should show the button again after the mod is disabled", function () {
+            document.mod.getHideButtonSetting = () => true;
+            const dummyButton = document.createElement("div");
+            document.mod.getAllContentButton = () => dummyButton;
+            document.mod.setButtonVisibility(true);
+            document.mod.setButtonVisibility(false);
             assert.equal(dummyButton.style.display, "");
+        })
+    })
+
+    describe ("getTitle", function () {
+        it ("still retrieves the correct element", async function () {
+            document = await setup(modId, modClass, "https://kbin.social/m/kbinmeta");
+            /** @type {HTMLElement} */
+            const title = document.mod.getTitle();
+            assert.ok(title != null || title != undefined);
+            const prefix = title.parentElement.querySelector("span").innerHTML;
+            assert.equal(prefix, "/m/");
+            assert.equal(title.getAttribute("href"), "/m/kbinMeta");
+        })
+        it ("still retrieves the correct element when run multiple times", async function () {
+            document = await setup(modId, modClass, "https://kbin.social/m/kbinmeta");
+            document.mod.getHideButtonSetting = () => true;
+            document.mod.setup();
+            /** @type {HTMLElement} */
+            let title = document.mod.getTitle();
+            assert.ok(title != null || title != undefined);
+            const prefix = title.parentElement.querySelector("span").innerHTML;
+            assert.equal(prefix, "/m/");
+            assert.equal(title.getAttribute("href"), "/*/m/kbinMeta");
+        })
+    })
+
+    describe ("getAllContentButton", function () {
+        it ("still retrieves the correct element", async function () {
+            document = await setup(modId, modClass, "https://kbin.social/m/kbinmeta");
+            /** @type {HTMLElement} */
+            const button = document.mod.getAllContentButton();
+            assert.ok(button != null || button != undefined);
+            assert.equal(button.getAttribute("href"), "/*/m/kbinMeta");
+            assert.equal(button.parentElement.nodeName, "LI");
+        })
+        it ("still retrieves the correct element when run multiple times", async function () {
+            document = await setup(modId, modClass, "https://kbin.social/m/kbinmeta");
+            document.mod.getHideButtonSetting = () => true;
+            document.mod.setup();
+            /** @type {HTMLElement} */
+            const button = document.mod.getAllContentButton();
+            assert.ok(button != null || button != undefined);
+            assert.equal(button.getAttribute("href"), "/*/m/kbinMeta");
+            assert.equal(button.parentElement.nodeName, "LI");
+        })
+    })
+
+    describe ("setup", function () {
+        it ("changes the magazine title's link correctly", function () {
+            const title = document.createElement("a");
+            title.setAttribute("href", "/m/example");
+            document.mod.getTitle = () => title;
+            document.mod.setButtonVisibility = () => null;
+            document.mod.setup();
+            assert.equal(title.getAttribute("href"), "/*/m/example");
+        })
+        it ("changes the magazine title's link correctly when run multiple times", function () {
+            const title = document.createElement("a");
+            title.setAttribute("href", "/m/example");
+            document.mod.getTitle = () => title;
+            document.mod.setButtonVisibility = () => null;
+            document.mod.setup();
+            document.mod.setup();
+            assert.equal(title.getAttribute("href"), "/*/m/example");
+        })
+        it ("calls the setButtonVisibility function", function () {
+            let called = undefined;
+            const title = document.createElement("a");
+            title.setAttribute("href", "/m/example");
+            document.mod.getTitle = () => title;
+            document.mod.setButtonVisibility = (isActive) => called = isActive;
+            document.mod.setup();
+            assert.equal(called, true);
+        })
+        it ("works in practice", async function() {
+            document = await setup(modId, modClass, "https://kbin.social/m/kbinmeta");
+            document.mod.getHideButtonSetting = () => true;
+            document.mod.setup();
+            assert.ok(document.mod.getTitle().getAttribute("href").startsWith("/*/"));
+            assert.ok(document.mod.getAllContentButton().style.display == "none");
+        })
+    })
+
+    describe ("teardown", function () {
+        it ("restores the original magazine title", function () {
+            const title = document.createElement("a");
+            title.setAttribute("href", "/m/example");
+            document.mod.getTitle = () => title;
+            document.mod.setButtonVisibility = () => null;
+            // to make sure we actually get the original one back, setup needs to be run first
+            document.mod.setup(); 
+            document.mod.teardown();
+            assert.equal(title.getAttribute("href"), "/m/example");
+        })
+        it ("calls the setButtonVisibility function", function () {
+            let called = undefined;
+            const title = document.createElement("a");
+            title.setAttribute("href", "/m/example");
+            document.mod.getTitle = () => title;
+            document.mod.setButtonVisibility = (isActive) => called = isActive;
+            document.mod.teardown();
+            assert.equal(called, false);
+        })
+        it ("works in practice", async function() {
+            document = await setup(modId, modClass, "https://kbin.social/m/kbinmeta");
+            document.mod.getHideButtonSetting = () => true;
+            const originalLink = document.mod.getTitle().getAttribute("href");
+            const originalStyle = document.mod.getAllContentButton().style.display;
+            document.mod.setup();
+            document.mod.teardown();
+            assert.equal(document.mod.getTitle().getAttribute("href"), originalLink);
+            assert.equal(document.mod.getAllContentButton().style.display, originalStyle);
         })
     })
 })
--- test/api.mjs
+++ test/api.mjs
@@ -2,11 +2,19 @@
 
 import { JSDOM } from "jsdom";
 
-/** @param {String} mod @param {String} main_function */
-export async function setup (mod, main_function) {
-    const html = 
-            `<script src="file:${process.cwd()}/mods/${mod}/${mod}.user.js"></script>`
-        +   `<script>document.mod = new ${main_function}();</script>`;
+/**
+ * @param {String} mod 
+ * @param {String} main_function 
+ * @param {String?} loadFromUrl
+*/
+export async function setup (mod, main_function, loadFromUrl = null) {
+    let html = "";
+    if (loadFromUrl != null) {
+        const response = await fetch(loadFromUrl);
+        html = await response.text();
+    }
+    html += `<script src="file:${process.cwd()}/mods/${mod}/${mod}.user.js"></script>`;
+    html += `<script>document.mod = new ${main_function}();</script>`;
     const jsdom = new JSDOM( html, { runScripts: "dangerously", resources: "usable" } );
     await new Promise((resolve) => jsdom.window.addEventListener("load", resolve));
     return jsdom.window.document;
