1842c9c7b4e1695002026f99965b070cc40c20ee
Pamasich <pamasich@gmail.com>
2024-02-14 13:32:26 +0100

    test(fix-codeblocks): redid style pattern tests

--- test/fix-codeblocks.mjs
+++ test/fix-codeblocks.mjs
@@ -7,52 +7,54 @@ import { setup } from "./api.mjs";
 /** @type {Document} */
 let document = undefined;
 
-describe ("fix-codeblocks", function () {
-    beforeEach (async function () {
-        document = await setup("fix-codeblocks", "LemmyCodeFixer");
-    });
+const modId = "fix-codeblocks";
+const modClass = "FixLemmyCodeblocksMod";
 
-    describe ("stylePattern", function () {
-        it ("should exist", function () {
-            assert.ok(document.mod.stylePattern != undefined);
-            assert.ok(document.mod.stylePattern != null);
-            assert.ok(document.mod.stylePattern != "");
-        });
-        it ("should match any hexadecimal color with no additional properties", function () {
-            const randomColor = new RandExp(/[0-9a-fA-F]{6}/);
-            assert.match(`color:#${randomColor.gen()};`, new RegExp(document.mod.stylePattern));
-            assert.match(`color:#${randomColor.gen()};`, new RegExp(document.mod.stylePattern));
-            assert.match(`color:#${randomColor.gen()};`, new RegExp(document.mod.stylePattern));
+describe ("fix-codeblocks", function () {
+    this.timeout(10000);
+    this.slow(1000);
+
+    beforeEach (async function () {
+        document = await setup(modId, modClass);
+        this.mod = document.mod;
+    })
+
+    describe ("getStylePattern", function () {
+        beforeEach (function () {
+            this.styleRegex = new RegExp(this.mod.getStylePattern());
+            this.randomColor = new RandExp(/[0-9a-fA-F]{6}/);
+        })
+
+        it ("should match any hexadecimal color", function () {
+            assert.match(`color:#${this.randomColor.gen()};`, this.styleRegex);
+            assert.match(`color:#${this.randomColor.gen()};`, this.styleRegex);
+            assert.match(`color:#${this.randomColor.gen()};`, this.styleRegex);
         })
         it ("should match a color preceeded by a bold style", function () {
-            const randomColor = new RandExp(/[0-9a-fA-F]{6}/).gen();
-            const css = `font-weight:bold;color:#${randomColor};`;
-            assert.match(css, new RegExp(document.mod.stylePattern));
+            const css = `font-weight:bold;color:#${this.randomColor.gen()};`;
+            assert.match(css, this.styleRegex);
         })
         it ("should match a color preceeded by an italic style", function () {
-            const randomColor = new RandExp(/[0-9a-fA-F]{6}/).gen();
-            const css = `font-style:italic;color:#${randomColor};`
-            assert.match(css, new RegExp(document.mod.stylePattern));
+            const css = `font-style:italic;color:#${this.randomColor.gen()};`
+            assert.match(css, this.styleRegex);
         })
         it ("should not match if other properties are present", function () {
             let style = 'color:#222222;background-color:#333333;';
-            assert.doesNotMatch(style, new RegExp(`^${document.mod.stylePattern}$`));
+            assert.doesNotMatch(style, new RegExp(`^${document.mod.getStylePattern()}$`));
             style = 'margin-left:3px;color:#434343;';
-            assert.doesNotMatch(style, new RegExp(`^${document.mod.stylePattern}$`));
+            assert.doesNotMatch(style, new RegExp(`^${document.mod.getStylePattern()}$`));
         })
         it ("should not match if the color is not given in hexadecimal", function () {
-            assert.doesNotMatch('color:rgb(255,255,255);', new RegExp(document.mod.stylePattern));
-            assert.doesNotMatch('color:hsl(0,100%,50%);', new RegExp(document.mod.stylePattern));
-            assert.doesNotMatch('color:red;', new RegExp(document.mod.stylePattern));
+            assert.doesNotMatch('color:rgb(255,255,255);', this.styleRegex);
+            assert.doesNotMatch('color:hsl(0,100%,50%);', this.styleRegex);
+            assert.doesNotMatch('color:red;', this.styleRegex);
         })
         it ("should not match if spaces are used", function () {
-            assert.doesNotMatch('color: #222222;', new RegExp(document.mod.stylePattern));
+            assert.doesNotMatch('color: #222222;', this.styleRegex);
         })
         it ("should not check for the start or end of a string", function () {
-            /** @type {String} */
-            const pattern = document.mod.stylePattern;
-            assert.ok(!pattern.startsWith('^'));
-            assert.ok(!pattern.endsWith('$'));
+            assert.ok(!document.mod.getStylePattern().startsWith('^'));
+            assert.ok(!document.mod.getStylePattern().endsWith('$'));
         })
     })
 })
