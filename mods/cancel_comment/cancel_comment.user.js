9de048af4c0ce63c13602d22c5bebdeebb0776a5
Pamasich <pamasich@gmail.com>
2024-06-14 15:49:16 +0200

    wip(cancel_comment): started work on adding a cancel button to the reply forms

---
+++ mods/cancel_comment/cancel_comment.user.js
@@ -0,0 +1,16 @@
+/**
+ * [mod description here]
+ * @param {boolean} isActive Whether the mod has been turned on
+ */
+function setupCancelReplyButton (isActive) { // eslint-disable-line no-unused-vars
+    if (isActive) setup();
+    else teardown();
+
+    function setup () {
+
+    }
+
+    function teardown () {
+
+    }
+}
