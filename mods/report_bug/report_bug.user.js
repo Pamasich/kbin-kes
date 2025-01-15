08bd401885b2e6a1f25152bd30d9198d8b742795
Pamasich <pamasich@gmail.com>
2025-01-15 14:32:26 +0100

    chore(mods): changed some mentions of kbin to mbin

--- mods/report_bug/report_bug.user.js
+++ mods/report_bug/report_bug.user.js
@@ -10,7 +10,7 @@ function bugReportInit (toggle) { // eslint-disable-line no-unused-vars
         let postID = item.getAttribute("id");
         let bareURL = window.location.href.split("#")[0];
         let originURL = bareURL + "%23" + postID;
-        let footer = `%0A%0AReposted from kbin:%0A${originURL}`;
+        let footer = `%0A%0AReposted from mbin:%0A${originURL}`;
         let postBody = item.querySelector('.content').innerText;
         let postFooter = item.querySelector('footer menu .dropdown ul');
         let newListItem = document.createElement('li');
