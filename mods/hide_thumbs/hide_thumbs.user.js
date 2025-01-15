85d578fdb3d34e07b0d6bfd519869662ee727a9f
Pamasich <pamasich@gmail.com>
2025-01-15 14:13:29 +0100

    chore(hide_thumbs)!: removed obsolete thread option
    Mbin has its own thumbnail hiding functionality for threads, so I've removed that option from the mod.

--- mods/hide_thumbs/hide_thumbs.user.js
+++ mods/hide_thumbs/hide_thumbs.user.js
@@ -1,12 +1,6 @@
 function hideThumbs (toggle) { //eslint-disable-line no-unused-vars
     const settings = getModSettings('hidethumbs')
-    const index = 'kes-index-thumbs'
     const inline = 'kes-inline-thumbs'
-    const thumbsCSS = `
-    .entry.section.subject figure, .no-image-placeholder {
-        display: none
-    }
-    `
     const inlineCSS = `
     .thumbs {
         display:none
@@ -20,18 +14,12 @@ function hideThumbs (toggle) { //eslint-disable-line no-unused-vars
         safeGM("removeStyle", name)
     }
     if (toggle) {
-        if (settings["index"]) {
-            apply(thumbsCSS, index);
-        } else {
-            unset(index)
-        }
         if (settings["inline"]) {
             apply(inlineCSS, inline)
         } else {
             unset(inline)
         }
     } else {
-        unset(index)
         unset(inline)
     }
 }
