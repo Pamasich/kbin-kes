84d95e7464bd2a1ce063e58539b9d1e8c16c28b5
Pamasich <pamasich@gmail.com>
2025-03-07 16:14:53 +0100

    chore(federation_warning): removed header and renamed kbin to mbin in description

--- mods/move_federation_warning/move_federation_warning.user.js
+++ mods/move_federation_warning/move_federation_warning.user.js
@@ -1,20 +1,4 @@
 function moveFederationWarningEntry (toggle) { //eslint-disable-line no-unused-vars
-    // ==UserScript==
-    // @name         Kbin: Move federation alert
-    // @match        https://kbin.social/*
-    // @match        https://lab2.kbin.pub/*
-    // @match        https://lab3.kbin.pub/*
-    // @match        https://fedia.io/*
-    // @match        https://karab.in/*
-    // @match        https://kbin.cafe/*
-    // @version      1.0
-    // @description  Moves the magazine federation warning to the sidebar's magazine info panel
-    // @author       PrinzKasper
-    // @namespace    https://github.com/jansteffen
-    // @icon         https://kbin.social/favicon.svg
-    // @license      MIT
-    // ==/UserScript==
-
     const loc = window.location.href.split('/')
     // only run on magazine, profile, and "all content" pages
     if ((loc[3] !== "m") && (loc[3] !== "u") && (loc[3] !== "*")) return;
