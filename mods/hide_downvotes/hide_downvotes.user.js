6572c5bfd4f9e3beb3a7b46c998a2e5e01690dc7
Pamasich <pamasich@gmail.com>
2025-03-07 14:54:14 +0100

    chore(hide_downvotes): removed header

--- mods/hide_downvotes/hide_downvotes.user.js
+++ mods/hide_downvotes/hide_downvotes.user.js
@@ -1,13 +1,4 @@
 function hideDownvotes (toggle) { // eslint-disable-line no-unused-vars
-    // ==UserScript==
-    // @name         kbin Vote Hider
-    // @namespace    https://github.com/aclist
-    // @version      0.2
-    // @description  Hide upvotes, downvotes, and karma
-    // @author       artillect
-    // @match        https://kbin.social/*
-    // @license      MIT
-    // ==/UserScript==
     if (toggle) {
         $('form.vote__down').hide();
     } else {
