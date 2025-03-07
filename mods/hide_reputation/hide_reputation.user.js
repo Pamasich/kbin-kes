84ebf3f079f29f72ff46750955a0e1033d28cbab
Pamasich <pamasich@gmail.com>
2025-03-07 14:59:13 +0100

    chore(hide_reputation): removed header

--- mods/hide_reputation/hide_reputation.user.js
+++ mods/hide_reputation/hide_reputation.user.js
@@ -1,13 +1,4 @@
 function hideReputation (toggle) { //eslint-disable-line no-unused-vars
-    // ==UserScript==
-    // @name         kbin Vote Hider
-    // @namespace    https://github.com/aclist
-    // @version      0.3
-    // @description  Hide upvotes, downvotes, and karma
-    // @author       artillect
-    // @match        https://kbin.social/*
-    // @license      MIT
-    // ==/UserScript==
     const itemSelector = 'li:has(a[href$="/reputation/threads"])'
     if (toggle) {
         $(`#sidebar > section.section.user-info > ul > ${itemSelector}`).hide()
